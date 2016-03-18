54b503426eb641cecd71b038ef28202822494de9
lhog <ccna@mail.ru>
2016-03-18 16:01:34 +0300

    Sniper changes: Spectre --> Sniper minCloakDistance 155 --> 185 (+20%) damage 1500 --> 1800 (+20%) reload time 17 --> 20 (+18%) projectile speed 850 --> 1275 (+50%)

--- units/armsnipe.lua
+++ units/armsnipe.lua
@@ -1,6 +1,6 @@
 unitDef = {
   unitname               = [[armsnipe]],
-  name                   = [[Spectre]],
+  name                   = [[Sniper]],
   description            = [[Cloaked Skirmish/Anti-Heavy Artillery Bot]],
   acceleration           = 0.3,
   brakeRate              = 0.2,
@@ -28,13 +28,13 @@ unitDef = {
     description_fi = [[Tarkka-ampujarobotti]],
     description_fr = [[Marcheur Sniper]],
     description_it = [[Camminatore Cecchino]],
-    helptext       = [[The Spectre's energy rifle inflicts heavy damage to a single target. It can fire while cloaked; however its visible round betrays its position. It requires quite a bit of energy to keep cloaked, especially when moving. The best way to locate a Spectre is by sweeping the area with many cheap units.]],
-    helptext_bp    = [[Spectre ? uma unidade de artilharia invis?vel a radar e capaz de se camuflar. Pode atirar enquanto camuflado, mas seus tiros vis?veis indicam sua posi?ao. Requer muita energia para manter camuflado e atirando. Quando destru?do, uma onda de PEM paraliza unidades pr?ximas. Usado melhor s?zinho.]],
+    helptext       = [[The Sniper's energy rifle inflicts heavy damage to a single target. It can fire while cloaked; however its visible round betrays its position. It requires quite a bit of energy to keep cloaked, especially when moving. The best way to locate a Sniper is by sweeping the area with many cheap units.]],
+    helptext_bp    = [[Sniper ? uma unidade de artilharia invis?vel a radar e capaz de se camuflar. Pode atirar enquanto camuflado, mas seus tiros vis?veis indicam sua posi?ao. Requer muita energia para manter camuflado e atirando. Quando destru?do, uma onda de PEM paraliza unidades pr?ximas. Usado melhor s?zinho.]],
     helptext_de    = [[Sein energetisches Gewehr richtet riesigen Schaden bei einzelnen Zielen an. Er kann auch schießen, wenn er getarnt ist. Dennoch verrät ihn sein sichtbarer Schuss. Um getarnt zu bleiben und schießen zu können, benötigt der Scharfschütze eine Menge Energie. Die einfachst Möglichkeit einen Scharfschützen ausfindig zu machen, ist die, indem man ein Gebiet mit vielen billigen überschwemmt.]],
-    helptext_es    = [[El Spectre es una unidad ocultada de artilleria costosa. Puede disparar mientras ocultado, pero la bala visible revela su posición. Necesita mucha energía para disparar mientras ocultado. La mejor manera de encontrar un Spectre es de ispeccionar el área con unidades de exploración.]],
-    helptext_fi    = [[Kallis, vihollisen tutkaan ilmaantumaton ja n?kym?tt?myyskentt?? hy?dynt?v? Spectre on tehokas erikoisteht?vien yksikk?. Specterin korkeaenergisen, pitk?n kantaman pulssiplasma-aseen k?ytt?minen sek? n?kym?tt?m?n? pit?minen vaativat paljon energiaa. Tehokkain tapa paikantaa t?m? yksikk? on l?hett?? lukuisia tiedustelijoita alueelle.]],
-    helptext_fr    = [[Le Spectre est une unit? d'artillerie furtive, camouflable et coutant tres cher. Il peut faire feu tout en restant camoufl?. Son tir tres visible peut cependant r?veler sa position. La quantit?e d'?nergie qu'il n?cessite pour tirer et rester camoufler en m?me temps est ?lev?e. Sa destruction ?met une onde de choque EMP qui immobilise les unit?s qui se trouve a proximit?. Il est le plus utile en tant que tireur isol?.]],
-    helptext_it    = [[Il Spectre é un unita costosa occultata d'artiglieria. Puo sparare mentre é occultata; pero il proiettile visibile tradisce la sua posizione. Richiede molta energia per tenerlo occultato mentre spara. la migliore maniera di trovare un Spectre é di ispezionare l'area con unita di ricognizione]],
+    helptext_es    = [[El Sniper es una unidad ocultada de artilleria costosa. Puede disparar mientras ocultado, pero la bala visible revela su posición. Necesita mucha energía para disparar mientras ocultado. La mejor manera de encontrar un Sniper es de ispeccionar el área con unidades de exploración.]],
+    helptext_fi    = [[Kallis, vihollisen tutkaan ilmaantumaton ja n?kym?tt?myyskentt?? hy?dynt?v? Sniper on tehokas erikoisteht?vien yksikk?. Specterin korkeaenergisen, pitk?n kantaman pulssiplasma-aseen k?ytt?minen sek? n?kym?tt?m?n? pit?minen vaativat paljon energiaa. Tehokkain tapa paikantaa t?m? yksikk? on l?hett?? lukuisia tiedustelijoita alueelle.]],
+    helptext_fr    = [[Le Sniper est une unit? d'artillerie furtive, camouflable et coutant tres cher. Il peut faire feu tout en restant camoufl?. Son tir tres visible peut cependant r?veler sa position. La quantit?e d'?nergie qu'il n?cessite pour tirer et rester camoufler en m?me temps est ?lev?e. Sa destruction ?met une onde de choque EMP qui immobilise les unit?s qui se trouve a proximit?. Il est le plus utile en tant que tireur isol?.]],
+    helptext_it    = [[Il Sniper é un unita costosa occultata d'artiglieria. Puo sparare mentre é occultata; pero il proiettile visibile tradisce la sua posizione. Richiede molta energia per tenerlo occultato mentre spara. la migliore maniera di trovare un Sniper é di ispezionare l'area con unita di ricognizione]],
 	modelradius    = [[15]],
 	dontfireatradarcommand = '1',
   },
@@ -53,7 +53,7 @@ unitDef = {
   maxSlope               = 36,
   maxVelocity            = 1.45,
   maxWaterDepth          = 22,
-  minCloakDistance       = 155,
+  minCloakDistance       = 185,
   movementClass          = [[KBOT2]],
   moveState              = 0,
   noChaseCategory        = [[TERRAFORM FIXEDWING GUNSHIP SUB TOOFAST CHEAP]],
@@ -104,8 +104,8 @@ unitDef = {
 	  },
 	  
       damage                  = {
-        default = 1500.1,
-        planes  = 1500.1,
+        default = 1800.1,
+        planes  = 1800.1,
         subs    = 75,
       },
 
@@ -116,7 +116,7 @@ unitDef = {
       interceptedByShieldType = 1,
       noSelfDamage            = true,
       range                   = 700,
-      reloadtime              = 17,
+      reloadtime              = 20,
       rgbColor                = [[1 0.2 0.2]],
       separation              = 1.5,
       size                    = 5,
@@ -125,7 +125,7 @@ unitDef = {
       soundStart              = [[weapon/gauss_fire]],
       turret                  = true,
       weaponType              = [[Cannon]],
-      weaponVelocity          = 850,
+      weaponVelocity          = 1275,
     },
 
   },
@@ -133,7 +133,7 @@ unitDef = {
   featureDefs            = {
 
     DEAD = {
-      description      = [[Wreckage - Spectre]],
+      description      = [[Wreckage - Sniper]],
       blocking         = true,
       damage           = 560,
       energy           = 0,
@@ -147,7 +147,7 @@ unitDef = {
     },
 
     HEAP = {
-      description      = [[Debris - Spectre]],
+      description      = [[Debris - Sniper]],
       blocking         = false,
       damage           = 560,
       energy           = 0,
