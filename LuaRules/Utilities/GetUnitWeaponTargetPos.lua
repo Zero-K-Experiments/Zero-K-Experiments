c55bd7fcfce1d27bd8b05b103b51b55f1db49c69
lhog <ccna@mail.ru>
2016-05-04 22:56:09 +0300

    Added LuaRules/Utilities/GetUnitWeaponTargetPos.lua

--- LuaRules/Utilities/GetUnitWeaponTargetPos.lua
+++ LuaRules/Utilities/GetUnitWeaponTargetPos.lua
@@ -4,9 +4,7 @@ local spGetUnitPosition = Spring.GetUnitPosition
 
 local function GetUnitWeaponTargetPos(unitID, wNum)
 	local x, y, z
-	local res, isUserTarget, xVal = spGetUnitWeaponTarget(unitID, attackerWeaponNum)
-	
-	assert(res>0)
+	local res, isUserTarget, xVal = spGetUnitWeaponTarget(unitID, wNum)	
 
 	if res == 1 then --unit
 		_, _, _, x, y, z = spGetUnitPosition(xVal, false, true) --aimpos
@@ -14,9 +12,11 @@ local function GetUnitWeaponTargetPos(unitID, wNum)
 		x, y, z = spGetProjectilePosition(xVal)
 	elseif res == 2 then --ground
 		x, y, z = xVal[1], xVal[2], xVal[3]
+	else
+		x, y, z, xVal, isUserTarget = nil, nil, nil, nil, nil
 	end
 	
-	return x, y, z, res, isUserTarget
+	return x, y, z, res, xVal, isUserTarget
 end
 
 Spring.Utilities.GetUnitWeaponTargetPos = GetUnitWeaponTargetPos
